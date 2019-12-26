d3dca4a6093fc18ba674139f8f3bef1623d161fb
Ian Wolterstorff <wolterstorff.i@husky.neu.edu>
2019-12-25 23:47:41 -0500

    Day 1 prob 2

--- day1.hs
+++ day1.hs
@@ -6,14 +6,30 @@ main :: IO ()
 main = do
     [fileName, probNum] <- getArgs
     fileContents <- readFile fileName
+    let input = stringInputToInts fileContents
     case probNum of
-        "1" -> print (prob1 fileContents)
+        "1" -> print (prob1 input)
+        "2" -> print (prob2 input)
         _ -> error "Invalid probnum"
 
-prob1 :: String -> Int
-prob1 moduleMasses =
-    let input = stringInputToInts moduleMasses
-    in sum (map (\mass -> mass `div` 3 - 2) input)
+prob1 :: [Int] -> Int
+prob1 input =
+    sum (map fuelForModule input)
+
+prob2 :: [Int] -> Int
+prob2 input = 
+    sum (map (\mass -> fuelForModuleComplete mass 0) input)
+
+fuelForModule :: Int -> Int
+fuelForModule mass =
+    if fuelMass >= 0 then fuelMass else 0
+    where fuelMass = mass `div` 3 - 2
+
+fuelForModuleComplete :: Int -> Int -> Int
+fuelForModuleComplete 0 acc = acc
+fuelForModuleComplete mass acc =
+    fuelForModuleComplete fuel (acc + fuel)
+    where fuel = fuelForModule mass
 
 stringInputToInts :: String -> [Int]
 stringInputToInts bigstring =
