39bd0a81fff235caa9571decdd9a3e58c34a5726
Ian Wolterstorff <wolterstorff.i@husky.neu.edu>
2019-12-27 15:22:16 -0500

    Day 2 part 1 complete

--- Advent/Day2.hs
+++ Advent/Day2.hs
@@ -1,29 +1,47 @@
 module Advent.Day2 (
-day21
+day21,
+inputToMap,
+interpretIntcode
 ) where
 
+import Debug.Trace
+
 import Data.List.Split
+import qualified Data.Map as Map
 
 day21 :: String -> String
-day21 input =
-    show $ head (interpretIntcode (replaceNth (replaceNth code 1 12) 2 2) 0)
-    where code = map read $ splitOn "," input
+day21 input = 
+    let inCode = Map.insert 1 12 .
+                Map.insert 2 2 $ inputToMap input
+        result = interpretIntcode inCode
+    in show $ result Map.! 0
 
-interpretIntcode :: [Int] -> Int -> [Int]
-interpretIntcode code pointer
-    | opcode == 99 = code
-    | opcode == 1 = intcodeOp (+) code param1 param2 resPnt
-    | opcode == 2 = intcodeOp (*) code param1 param2 resPnt
-    where opcode = code !! pointer
-          param1 = code !! (pointer + 1)
-          param2 = code !! (pointer + 2)
-          resPnt = code !! (pointer + 3)
+inputToMap :: String -> Map.Map Int Int
+inputToMap input =
+    let inList = splitOn "," input
+        assocList = zip [0..(length inList - 1)] $ map read inList
+    in Map.fromList assocList
 
-intcodeOp :: (Int -> Int -> Int) -> [Int] -> Int -> Int -> Int -> [Int]
-intcodeOp fun code param1 param2 resPnt =
-    replaceNth code resPnt (fun param1 param2)
+interpretIntcode :: Map.Map Int Int -> Map.Map Int Int
+interpretIntcode intcode =
+    interpretIntcodeAux intcode 0
 
-replaceNth :: [a] -> Int -> a -> [a]
-replaceNth xs ind elem =
-    let (x,_:ys) = splitAt ind xs
-    in x ++ (elem : ys)
+interpretIntcodeAux :: Map.Map Int Int -> Int -> Map.Map Int Int
+--interpretIntcodeAux intcode pointer | trace ("pointer = " ++ show pointer ++ " intcode = " ++ show intcode) False = undefined
+interpretIntcodeAux intcode pointer
+    | Map.notMember pointer intcode = error "you dun goofed"
+    | inst == 99    = intcode
+    | inst == 1     = interpretIntcodeAux (interpretFn (+)) (pointer + 4)
+    | inst == 2     = interpretIntcodeAux (interpretFn (*)) (pointer + 4)
+    | otherwise     = error "you double goofed"
+    where inst = intcode Map.! pointer
+          param1 = intcode Map.! (intcode Map.! (pointer + 1)) -- Fetch the first operand
+          param2 = intcode Map.! (intcode Map.! (pointer + 2)) -- Fetch the second operand
+          res = intcode Map.! (pointer + 3) -- The location to place the result
+          interpretFn = applyInstruction param1 param2 res intcode
+
+
+applyInstruction :: Int -> Int -> Int -> Map.Map Int Int -> (Int -> Int -> Int) -> Map.Map Int Int
+--applyInstruction param1 param2 res intcode fun | trace ("function applied to " ++ show param1 ++ " " ++ show param2 ++ " stored in res " ++ show res) False = undefined
+applyInstruction param1 param2 res intcode fun =
+    Map.insert res (fun param1 param2) intcode
--- Spec.hs
+++ Spec.hs
@@ -1,9 +1,43 @@
 import Test.Hspec
 
+import qualified Data.Map as Map
+
 import Utils
+import Advent.Day1
+import qualified Advent.Day2 as Day2
 
 main :: IO ()
 main = hspec $ do
     describe "Utils.stringInputToInts" $ do
         it "constructs a list of integers from a string" $ do
             stringInputToInts "1\n2\n3\n4\n" `shouldBe` [1,2,3,4]
+
+    describe "Advent.Day1" $ do
+        describe "day11" $ do
+            it "calculates fuel mass" $ do
+                day11 "12" `shouldBe` "2"
+                day11 "14" `shouldBe` "2"
+                day11 "1969" `shouldBe` "654"
+                day11 "100756" `shouldBe` "33583"
+                day11 "12\n14\n1969\n100756" `shouldBe` (show $ 2 + 2 + 654 + 33583)
+
+        describe "day12" $ do
+            it "calculates complete fuel mass" $ do
+                day12 "14" `shouldBe` "2"
+                day12 "1969" `shouldBe` "966"
+                day12 "100756" `shouldBe` "50346"
+                day12 "14\n1969\n100756" `shouldBe` (show $ 2 + 966 + 50346)
+
+    describe "Advent.Day2" $ do
+        describe "inputToMap" $ do
+            it "translates comma-separated input to Map" $ do
+                Day2.inputToMap "1,2,3,4" `shouldBe` Map.fromList [(0,1), (1,2), (2,3), (3,4)]
+
+        let ex1 = Day2.inputToMap "1,9,10,3,2,3,11,0,99,30,40,50"
+        let ex1Res = Day2.inputToMap "3500,9,10,70,2,3,11,0,99,30,40,50"
+
+        describe "interpretIntcode" $ do
+            it "interprets intcode sequences" $ do
+                Day2.interpretIntcode ex1 `shouldBe` ex1Res
+            
+            
